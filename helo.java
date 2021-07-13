ee80c23bf5cd614ce3ceacaa30baecbbc211cd7a
haamith <haamithasb@gmail.com>
2021-07-13 10:52:32 +0000

    commiting from child

--- helo.java
+++ helo.java
@@ -2,7 +2,7 @@ class Hello
 {
 public static void main(String args[])
 {
-System.out.println("third commit ,changing author information");
+System.out.println(" commit from child ,changing author information");
 }
 }
 
