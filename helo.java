08fc8520c68714e014b0cddb4b7336a4b72097a0
Haamith <87229884+Haamith@users.noreply.github.com>
2021-07-13 17:21:15 +0530

    committing from remote
    committing from remote

--- helo.java
+++ helo.java
@@ -2,7 +2,7 @@ class Hello
 {
 public static void main(String args[])
 {
-System.out.println(" commit from remote branch ,changing author information");
+System.out.println(" commit from remote branch 2nd time,changing author information");
 }
 }
 
